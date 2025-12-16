91583cc930105904d19a34b1c33bb4da601077c1
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-12-16 17:03:22 +0545

    [ADDED]: added loading indicator in payment initiated

--- app/(components)/EsewaPayment.tsx
+++ app/(components)/EsewaPayment.tsx
@@ -22,14 +22,14 @@ const EsewaPayment = () => {
     },
   });
 
-  const [pending, startTransition] = useTransition()
+  const [pending, startTransition] = useTransition();
 
   const handleEsewaPayment: SubmitHandler<EsewaPaymentFormType> = (data) => {
     console.log("data", data);
-    startTransition(async() => {
-       const url = await initiatePayment(data)
-        window.location.href = url.url;
-    })
+    startTransition(async () => {
+      const url = await initiatePayment(data);
+      window.location.href = url.url;
+    });
   };
 
   return (
@@ -70,12 +70,36 @@ const EsewaPayment = () => {
       </div>
 
       <div className="form-actions flex flex-col gap-[1.5rem]">
-        <button type="submit" className="btn bg-green-700 text-white p-[1.2rem_2rem] rounded-xl text-[1.8rem]">
-          Pay
-        </button>
-        <button type="reset" className="btn text-green-700 text-[1.8rem]">
-          Cancel
-        </button>
+        {pending ? (
+          <>
+            <button
+              type="button"
+              className="btn bg-green-700 text-white p-[1.2rem_2rem] rounded-xl text-[1.8rem] pointer-events-none cursor-not-allowed flex items-center justify-center"
+            >
+              <span className="icon-container w-[2.5rem] h-[2.5rem] flex items-center justify-center animate-spin">
+                <svg
+                  className="fill-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 640"
+                >
+                  <path d="M286.7 96.1C291.7 113 282.1 130.9 265.2 135.9C185.9 159.5 128.1 233 128.1 320C128.1 426 214.1 512 320.1 512C426.1 512 512.1 426 512.1 320C512.1 233.1 454.3 159.6 375 135.9C358.1 130.9 348.4 113 353.5 96.1C358.6 79.2 376.4 69.5 393.3 74.6C498.9 106.1 576 204 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320C64 204 141.1 106.1 246.9 74.6C263.8 69.6 281.7 79.2 286.7 96.1z" />
+                </svg>
+              </span>
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              type="submit"
+              className="btn bg-green-700 text-white p-[1.2rem_2rem] rounded-xl text-[1.8rem]"
+            >
+              Pay
+            </button>
+            <button type="reset" className="btn text-green-700 text-[1.8rem]">
+              Cancel
+            </button>
+          </>
+        )}
       </div>
     </form>
   );
